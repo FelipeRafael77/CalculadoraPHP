0054d18c9c24c9e35e2f0418dc90a35a0288c1e2
FelipeRafael77 <felipe_rafael900@outlook.com>
2018-08-12 15:20:18 -0300

    ADD funcao campotexto e css

--- calc.php
+++ calc.php
@@ -1,41 +1,24 @@
 <?php
-        # classe :: Calculadora
-        class Calculadora {
-
-            # Função "Calcular" para executar o cálculo dos valores (v1 e v2)
-            public function Calcular() {
-
-                # Se for setado algum valor ào submit (doCalc), executa a operação
-                if (isset($_POST['doCalc'])) {
-
-                    # Se a operação for soma (value = soma), então...
-                    if ($_POST['operacao'] == "soma") {
-
-                        # Armazena a soma de [v1 + v2] na variável $resultado
-                        $resultado = $_POST['v1'] + $_POST['v2'];
-
-                        # Exibe a variável $resultado com os valores já somados
-                        return $resultado;
-
-                        # Ou então, se a operação não for (value = soma), e sim (value = subtrai) então...
-                    } elseif ($_POST['operacao'] == "subtrai") {
-                        $resultado = $_POST['v1'] - $_POST['v2'];
-                        return $resultado;
-                    } elseif ($_POST['operacao'] == 'multiplica') {
-                        $resultado = $_POST['v1'] * $_POST['v2'];
-                        return $resultado;
-                    } elseif ($_POST['operacao'] == 'divide') {
-                        $resultado = $_POST['v1'] / $_POST['v2'];
-                        return $resultado;
-                    }
-                }
-            }
-
-        }
-
-        # Instancia a classe CALCULADORA()
-        $calcular = new Calculadora();
-
-        # Executa a função
-        echo $calcular->Calcular();
-        ?>
+   
+    function calcular($tp)
+    {
+       
+     $txtcaixa1 = (float) $_POST['txtcaixa1'];
+     $txtcaixa22 = (float) $_POST['txtcaixa2'];
+   
+    $resultado = 0;
+       
+    if ($tp == 1){
+        $resultado = $txtcaixa1 + $txtcaixa2;
+       
+    }else if ($tp == 2){
+        $resultado = (float) ($txtcaixa1 - $txtcaixa2);
+       
+    }else if ($tp == 3){
+        $resultado = (float) ($txtcaixa1 * $txtcaixa2);
+       
+    }else if ($tp == 4){
+        $resultado = (float) ($txtcaixa1 / $txtcaixa2);
+       
+    }
+}
--- index.php
+++ index.php
@@ -1,38 +1,47 @@
-<!DOCTYPE HTML>
-<html lang = "pt-br">
+<!DOCTYPE html>
+<html>
 <head>
-   <title>Exemplo</title>
-   <meta charset = "UTF-8">
-</head>
-<body>
-   <form action="" method="post" >
-      Primeiro Numero: <input name="num1" type="text"><br>
-      Segundo numero: <input name="num2" type="text"><br>
-      <input type="submit" name="operacao" value="+">     
-      <input type="submit" name="operacao" value="-">     
-      <input type="submit" name="operacao" value="*">     
-      <input type="submit" name="operacao" value="/">     
-   </form> 
-<?php
-
-   $a = $_POST['num1'];
-   $b = $_POST['num2'];
-   $op= $_POST['operacao'];
-
-   if( !empty($op) ) {
-      if($op == '+')
-         $c = $a + $b;
-      else if($op == '-')
-         $c = $a - $b;
-      else if($op == '*')
-         $c = $a*$b;
-      else
-         $c = $a/$b;
-
-      echo "O resultado da opera&ccedil;&atilde;o &eacute;: $c";
-   }
-
-?>       
+<title>Calculadora Básica em PHP</title>
+<meta charset="utf-8">
+<script type="text/javascript" src="js\campotexto.js"></script>
+<link rel="stylesheet" type="text/css" href="css\calc.css">
+<center>
+<h1>CalcPHP</h1>
+<form name="f1"><br>
+<input type="text" name="txtcaixa1">
+<br>
+<input type="text" name="txtcaixa2" value="0">
+<br>
+<br>
+<input type="button" class="clear" onclick="document.f1.txtcaixa2.value ='0'" value="CE">
+<input type="button" class="clear" onclick="document.f1.reset(); blocdel = false;" value="C">
+<input type="button" class="clear" onclick="deletecarac()" value="◄">
+ 
+<input type="button" class="divisao" onclick="divisao('/')" value="÷">
+<br>
+<input type="button" onclick="escrever(this.value)" value="7">
+<input type="button" onclick="escrever(this.value)" value="8">
+<input type="button" onclick="escrever(this.value)" value="9">
+<input type="button" class="multiplicacao" onclick="multiplicacao('*')" value="×">
+<br>
+<input type="button" onclick="escrever(this.value)" value="4">
+<input type="button" onclick="escrever(this.value)" value="5">
+<input type="button" onclick="escrever(this.value)" value="6">
+<input type="button" class="subtracao" onclick="subtracao('-')" value="-">
+<br>
+<input type="button" onclick="escrever(this.value)" value="1">
+<input type="button" onclick="escrever(this.value)" value="2">
+<input type="button" onclick="escrever(this.value)" value="3">
+<input type="button" class="soma" onclick="soma('+')" value="+">
+<br>
+<input type="button" class="maismenos" onclick="maismenos()" value="±">
+<input type="button" onclick="escrever(this.value)" value="0">
+<input type="button" onclick="escrever('.')" value=".">
+<input type="button" class="igual" onclick="calcular()" value="=">
+<br>
+</form>
+</center>
 </body>
+</head>
 </html>
 
